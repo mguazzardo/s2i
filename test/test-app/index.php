5f6ffd904aaad193de4d351335fea081134c6b91
mguazzardo <mguazzardo76@gmail.com>
2020-08-01 02:48:26 +0000

    subiendo logos v2

--- test/test-app/index.php
+++ test/test-app/index.php
@@ -3,7 +3,7 @@
 	<title>Examen banco galicia</title>
 </head>
 <body>
-<img src="logo-galicia.png" border="1" alt="Este es el ejemplo de un texto alternativo" width="400" height="300">
+<img src="logo-galicia.png" border="1" alt="Este es el ejemplo de un texto alternativo">
 <h1>Hola mundo banco galicia</h1>
 </body>
 </html>
